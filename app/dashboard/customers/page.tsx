5de6b5418b380a6a6ee348d049ead8d84300a507
haydar <haydarjoumaa@gmail.com>
2023-12-02 16:35:06 +0200

    small-fixes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+const CustomersPage = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+};
+export default CustomersPage;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+const DashboardPage = () => {
+  return <>Hello</>;
+};
+export default DashboardPage;
